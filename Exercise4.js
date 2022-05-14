1cde6b9467c0582fb4f48f1103b47a2fac5f38d4
Nigel Mdletshe <nigelmdletshe@gmail.com>
2022-05-14 17:35:34 +0200

    Added Student Object

--- Exercise4.js
+++ Exercise4.js
@@ -6,3 +6,23 @@ const convertToCelcius = function (a) {
 
 const result = convertToCelcius(200)
 console.log(result)
+
+// Objects
+const student = {
+  name: 'Kwezi',
+  studentNumber: 453528
+}
+
+console.log(student) // print the entire object
+
+// access the object's properties
+console.log(`${student.name}' student number is ${student.studentnumber}`)
+
+// Add a new property to the student
+const addAge = function (theStudent, age) {
+  theStudent.age = age
+}
+
+addAge(student, 20)
+
+console.log(student)
