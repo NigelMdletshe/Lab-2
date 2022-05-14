5fe9d663a1a17d29a090a957c81106654eca66c9
Nigel Mdletshe <nigelmdletshe@gmail.com>
2022-05-14 15:55:55 +0200

    added functions

--- exercise3.js
+++ exercise3.js
@@ -8,3 +8,13 @@ a += 2
 console.log(`a = ${a}, "a" is ${typeof a}`)
 a = true
 console.log(`a= ${a}, "a" is ${typeof a}`)
+
+const add = function (a, b) { // function expression assigned to add
+  return a + b
+}
+
+const result = add(2, 3) // execute the function
+console.log(result)
+
+const anotherAdd = add
+console.log(anotherAdd(12, 4)) // execute add again
