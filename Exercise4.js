020c31803ee848dd61849bc9734902fffd019b46
Nigel Mdletshe <nigelmdletshe@gmail.com>
2022-05-14 16:14:30 +0200

    createdafunction to convert Fahrenheit to degrees

---
+++ Exercise4.js
@@ -0,0 +1,8 @@
+'use strict'
+// function to convert Fahrenheit to degrees
+const convertToCelcius = function (a) {
+  return (a - 32) * 5 / 9
+}
+
+const result = convertToCelcius(200)
+console.log(result)
